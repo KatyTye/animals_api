d0e21bcb503c395d526031d1ca4ced0cad34be06
David Majholt <d.boy.dragoz@gmail.com>
2026-02-24 09:25:22 +0100

    feat: changed index.php for route usage and creation

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+// RECIVE ROUTER, DATABASE and RETURNS
+require_once("./db.php");
+require_once("./utils/returns.php");
+require_once("./utils/router.php");
 
-echo "Hello World!";
+// REGISTER RESOURCES
+require_once("./utils/resources.php");
+
+// GLOBAL VALUES
+$http_method = $_SERVER["REQUEST_METHOD"];
+
+// CREATE DEFAULT ROUTE
+createRoute(file_Path: "routes/docs", type: "docs");
+
+// CREATE ALL ROUTES
+createRoute("animals", "routes/animals/index", extra: true);
+
+// LOAD ROUTES
+loadRoutes();
