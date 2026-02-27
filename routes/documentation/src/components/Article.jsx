8ca5b07662ad18aebf918d778f4cb345cfc82504
David Majholt <d.boy.dragoz@gmail.com>
2026-02-27 10:29:41 +0100

    feat: added article component

---
+++ routes/documentation/src/components/Article.jsx
@@ -0,0 +1,18 @@
+export default function Article() {
+
+	return (
+		<article id="top" className="page-article">
+			<h2 className="page-article__title">
+				ABOUT THIS API
+			</h2>
+
+			<a href="https://github.com/KatyTye/animals_api" target="_blank">
+				Click here to open GitHub Repo
+			</a>
+
+			<p className="page-article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum fuga quod repellendus ab animi molestiae mollitia non eligendi ex, maiores quo possimus ipsum.</p>
+
+			<p className="page-article__text">Dolor sit amet consectetur adipisicing elit. Quas suscipit eveniet tenetur, hic iusto rerum nisi. Eos in sint esse, suscipit voluptatibus odit veritatis corrupti ipsum officiis id sit culpa eum possimus nisi earum eaque aliquid sunt molestias et fuga ea! Et qui natus architecto. Libero cumque corrupti sed. Quisquam.</p>
+		</article>
+	)
+}
