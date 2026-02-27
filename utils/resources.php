4971c5e45a760668812dff2ef898a842686719b8
David Majholt <d.boy.dragoz@gmail.com>
2026-02-27 09:48:16 +0100

    feat: added new css resource

--- utils/resources.php
+++ utils/resources.php
@@ -1,5 +1,11 @@
 <?php
 
 createResource("favicon", file_Type: "ico");
+
 createResource("fox", "images/fox");
-createResource("app", "routes/documentation/docs/app", file_Type: "js");
+
+createResource("app", "routes/documentation/docs/app",
+file_Type: "js");
+
+createResource("index", "routes/documentation/docs/index",
+file_Type: "css");
